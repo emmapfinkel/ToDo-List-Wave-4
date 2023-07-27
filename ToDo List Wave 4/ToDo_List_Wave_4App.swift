8b197bf8b4c1b07a503f1cc20073fb3aa919fb24
Emma Finkel <emma.finkel@gmail.com>
2023-07-27 11:41:45 -0400

    Initial Commit

---
+++ ToDo List Wave 4/ToDo_List_Wave_4App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_List_Wave_4App.swift
+//  ToDo List Wave 4
+//
+//  Created by scholar on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_List_Wave_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
